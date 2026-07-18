b2667583aed5db0f948a381a3ee04dc0dcbfb3fd
nihithasharanya <nihithasharanya@gmail.com>
2026-07-18 12:45:54 +0530

    random user id

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -27,3 +27,26 @@ function toggleUser(){
     var userGender= document.getElementById("user-gender");
     userGender.innerHTML = users[id].gender;
 }
+
+function randomuser(){
+    fetch('https://randomuser.me/api/')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+
+        var userdata = data.results[0];
+
+        var userImage = document.getElementById("user-image");
+    userImage.src = userdata.picture.large;
+
+    var userName= document.getElementsById("user-name");
+    userName.innerHTML = userdata.name.first + " " + userdata.name.last;
+
+    var userGender= document.getElementById("user-gender");
+    userGender.innerHTML = userdata.gender;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
