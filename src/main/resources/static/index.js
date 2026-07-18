d6256f7e870c1376d40adb075c5af916ce7281ef
nihithasharanya <nihithasharanya@gmail.com>
2026-07-18 12:07:43 +0530

    toggle id

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,29 @@
 // alert("Hellooo from js")
-console.log("hellooo from js")
+console.log("hellooo from js");
+
+var users=[
+    {
+        "name": "John Doe",
+        "gender": "Male",
+        "img": "john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender":"Female",
+        "img": "jane.png"
+    }
+]
+
+var id=0;
+
+function toggleUser(){
+    id=(id+1)%2
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    var userName= document.getElementsById("user-name");
+    userName.innerHTML = users[id].name;
+
+    var userGender= document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+}
